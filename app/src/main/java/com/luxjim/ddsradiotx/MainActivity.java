7f1835454fe3bba1c2e9ea659e9907a6925956d8
Jim <luxjim3763@gmail.com>
2016-03-27 16:46:00 +0200

    DDS Radio TX code update, 2016/03/27

--- app/src/main/java/com/luxjim/ddsradiotx/MainActivity.java
+++ app/src/main/java/com/luxjim/ddsradiotx/MainActivity.java
@@ -1,37 +1,90 @@
+/*
+                DDS Radio TX
+    Copyright (C) March 2016  Edouard Kanku
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Contact: info@luxjim.com
+ */
+
 package com.luxjim.ddsradiotx;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+import com.luxjim.ddsradiotx.driver.UsbSerialPort;
 
-    private TextView textview1, textview2, textview3;
+public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    private UsbSerialPort usbPort;
+    private int bufferSize = 8;
+    private String vid_pid_norm = "vid_04d8&pid_0011";
+    private long frequency = 1000000;
+    private long step = 100;
+    private boolean store = false;
+    Paint panel_paint = new Paint();
+    private TextView a,b,c,y;
+    private Button d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        Typeface SCRIPTBL = Typeface.createFromAsset(getAssets(), "androidnation.ttf");
+        Typeface FORTE = Typeface.createFromAsset(getAssets(), "Cornerstone.ttf");
+
+                //Initialize widgets...
+                a = (TextView) findViewById(R.id.freq_panel);
+        a.setTextSize(28);
+        a.setText("12.345,578");
+        panel_paint.setAntiAlias(true);
+        panel_paint.setColor(getResources().getColor(R.color.white));
+        a.setTypeface(SCRIPTBL);
+        b = (TextView) findViewById(R.id.textview_abst);
+        c = (TextView) findViewById(R.id.textview_schr);
+        y = (TextView) findViewById(R.id.freq_schrit);
+        y.setTextSize(20);
+        y.setText("Schrittweite 10000000");
+
+        d = (Button) findViewById(R.id.buttonplus);
+        e = (Button) findViewById(R.id.buttonmoins);
+        f = (Button) findViewById(R.id.buttonpgauche);
+        f.setText("<");
+        g = (Button) findViewById(R.id.buttondroit);
+        h = (Button) findViewById(R.id.button1);
+        i = (Button) findViewById(R.id.button2);
+        j = (Button) findViewById(R.id.button3);
+        k = (Button) findViewById(R.id.buttonAdd);
+        l = (Button) findViewById(R.id.button4);
+        m = (Button) findViewById(R.id.button5);
+        n = (Button) findViewById(R.id.button6);
+        o = (Button) findViewById(R.id.buttonSubtract);
+        p = (Button) findViewById(R.id.button7);
+        q = (Button) findViewById(R.id.button8);
+        r = (Button) findViewById(R.id.button9);
+        s = (Button) findViewById(R.id.buttonMultiply);
+        t = (Button) findViewById(R.id.buttonNegative);
+        u = (Button) findViewById(R.id.button0);
+        v = (Button) findViewById(R.id.buttonDot);
+        w = (Button) findViewById(R.id.buttonDivide);
+        x = (Button) findViewById(R.id.button111);
+
+    }
+
+
 }
